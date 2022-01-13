018c3b1a138e6e8be65a678e94c5f7efebeb03d6
Brooke Strand <brooke.strand007@gmail.com>
2022-01-12 19:42:12 -0500

    commit tree 9435a2cb897ec6dbfde89d630e3ecd1ee26dac84

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride<= 400) {
+    const freeSample = "This one is on me!";
+    return freeSample;
+  }
+  else if (ride >2000 && ride<2500) {
+    const payMe = "I will gladly take your thirty bucks.";
+    return payMe;
+  }
+  else { 
+    const nope = "No can do.";
+    return nope;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city == 'NYC') { 
+    const letsGo = 'Ok, sounds good.';
+    return letsGo;
+  }
+  else {
+    const nah = 'No go.';
+    return nah;
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if(tip =='generous') { 
+    const yay = "Thank you so much.";
+    return yay;
+  }
+  else if (tip == 'not as generous') {
+    const ehhh = "Thank you.";
+    return ehhh;
+  }
+  else {
+    const goodBye = "Bye.";
+    return goodBye;
+  }
 }
